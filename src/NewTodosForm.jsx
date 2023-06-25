5ed9ecbce91f624daa1290d27a6c6ee5becca04d
arigoogle <ariyantomail@gmail.com>
2023-06-26 06:13:56 +0700

    first commit

---
+++ src/NewTodosForm.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+export function NewTodoForm({onSubmit}) {
+  const [newItem, setNewItem] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (newItem === "") return
+    
+    onSubmit(newItem)
+
+    setNewItem('');
+  }
+
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className='new-item-form'
+      >
+        <div className='form-row'>
+          <label htmlFor=''>New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type='text'
+            id='item'
+            autoFocus
+          />
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+      
+    </>
+  );
+}
